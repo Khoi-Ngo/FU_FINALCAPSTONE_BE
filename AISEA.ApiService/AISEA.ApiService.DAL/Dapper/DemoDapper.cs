3bd080bdb43233a076b0dc23427bc5ac2c2b2500
Ngô Khôi <khoingo2003@gmail.com>
2025-06-13 14:51:00 +0700

    Note placeholder class Dapper

--- AISEA.ApiService/AISEA.ApiService.DAL/Dapper/DemoDapper.cs
+++ AISEA.ApiService/AISEA.ApiService.DAL/Dapper/DemoDapper.cs
@@ -7,6 +7,7 @@ namespace AISEA.ApiService.DAL.Dapper
 {
     public class DemoDapper
     {
+        //TODO: place holder class, remove later
         //writing a raw SQL query using Dapper
     }
 }

75d2183376a785901127baca9f22f68d4f3c9537
drekhai123 <khai.lumberjack@gmail.com>
2025-06-28 18:33:09 +0700

    config

--- AISEA.ApiService/AISEA.ApiService.DAL/EntityConfigurations/SubjectPrerequisiteConfiguration.cs
+++ AISEA.ApiService/AISEA.ApiService.DAL/EntityConfigurations/SubjectPrerequisiteConfiguration.cs
@@ -1,6 +1,11 @@
-using AISEA.ApiService.DAL.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using AISEA.ApiService.DAL.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AISEA.ApiService.DAL.EntityConfigurations
 {

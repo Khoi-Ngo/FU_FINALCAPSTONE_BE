17a4572702074a88f0cd4ddb92add90ec75f6851
drekhai123 <khai.lumberjack@gmail.com>
2025-07-08 17:42:19 +0700

    delete trash

--- AISEA.ApiService.SHARED/DTOs/Requests/Program/UpdateProgramRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace AISEA.ApiService.SHARED.DTOs.Requests.Program
-{
-    public class UpdateProgramRequest
-    {
-        public string ProgramCode { get; set; } = null!;
-        public string ProgramName { get; set; } = null!;
-    }
-}

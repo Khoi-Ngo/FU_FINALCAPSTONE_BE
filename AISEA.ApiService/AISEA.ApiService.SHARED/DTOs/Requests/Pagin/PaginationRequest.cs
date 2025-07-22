f0966098a64a045e85cb349dff85e567e1161f8d
Ngô Khôi <khoingo2003@gmail.com>
2025-07-22 20:17:39 +0700

    upscale limit for pagin request

--- AISEA.ApiService/AISEA.ApiService.SHARED/DTOs/Requests/Pagin/PaginationRequest.cs
+++ AISEA.ApiService/AISEA.ApiService.SHARED/DTOs/Requests/Pagin/PaginationRequest.cs
@@ -7,6 +7,6 @@ public class PaginationRequest
     [Range(1, int.MaxValue, ErrorMessage = "PageNumber must be at least 1.")]
     public int PageNumber { get; set; } = 1;
 
-    [Range(1, 20, ErrorMessage = "PageSize must be between 1 and 20.")]
+    [Range(1, 50, ErrorMessage = "PageSize must be between 1 and 50.")]
     public int PageSize { get; set; } = 10;
 }

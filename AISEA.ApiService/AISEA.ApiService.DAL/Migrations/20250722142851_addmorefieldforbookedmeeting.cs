610cecf504335806a970e85db63e49eb4465f912
Ngô Khôi <khoingo2003@gmail.com>
2025-07-22 21:29:54 +0700

    Exe the adding fields for BookedMeeting Table

---
+++ AISEA.ApiService/AISEA.ApiService.DAL/Migrations/20250722142851_addmorefieldforbookedmeeting.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AISEA.ApiService.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmorefieldforbookedmeeting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CheckinCode",
+                table: "BookedMeeting",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmCheckinCode",
+                table: "BookedMeeting",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentIssue",
+                table: "BookedMeeting",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SuggestionFromAdvisor",
+                table: "BookedMeeting",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TitleStudentIssue",
+                table: "BookedMeeting",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckinCode",
+                table: "BookedMeeting");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmCheckinCode",
+                table: "BookedMeeting");
+
+            migrationBuilder.DropColumn(
+                name: "ContentIssue",
+                table: "BookedMeeting");
+
+            migrationBuilder.DropColumn(
+                name: "SuggestionFromAdvisor",
+                table: "BookedMeeting");
+
+            migrationBuilder.DropColumn(
+                name: "TitleStudentIssue",
+                table: "BookedMeeting");
+        }
+    }
+}
--- AISEA.ApiService/AISEA.ApiService.SHARED/Const/Enums/EBooking.cs
+++ AISEA.ApiService/AISEA.ApiService.SHARED/Const/Enums/EBooking.cs
@@ -4,14 +4,21 @@ namespace AISEA.ApiService.SHARED.Const.Enums
     {
         PENDING = 1,
         CONFIRMED = 2,
-        ADV_CANCEL = 3,
-        STU_CANCEL = 9,
+        ADV_CANCELED = 3,
+        STU_CANCELED = 9,
         COMPLETED = 4,
         STUDENT_MISSED = 5,
         ADVISOR_MISSED = 6,
         NOT_APPROVED = 7,
         OVERDUE = 8,
 
+        //?The student can only book when there is no existed meeting in that time slot or STU_CANCELED only (Handle By Trigger Database)
+
+        //?The advisor can only log leave when there is no meeting in that time slot or if already had then those should be only incase (NOT_APPROVE, ADV_CANCELED, STU_CANCEL)
+
+        //? Other case need Advisor take action (Cancel them or do something similar like that ...) to be able to complete the logging leave (PENDING, CONFIRMED)
+
+        //? Other case will never happen if handle right(because only allow the advisor logging leave for the Time in the future): Create Leave when there is meeting (OVERDUE, ADVISOR_MISSED, STUDENT_MISSED, COMPLETED, )
     }
 
     public enum DayOfWeekAISEA

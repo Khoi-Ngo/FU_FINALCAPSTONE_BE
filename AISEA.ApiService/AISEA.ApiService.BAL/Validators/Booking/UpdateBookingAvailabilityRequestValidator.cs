fc97502f435cc1ebf08bb5975df80c490a500009
Ngô Khôi <khoingo2003@gmail.com>
2025-07-22 20:17:20 +0700

    constraint for update booking avai too

--- AISEA.ApiService/AISEA.ApiService.BAL/Validators/Booking/UpdateBookingAvailabilityRequestValidator.cs
+++ AISEA.ApiService/AISEA.ApiService.BAL/Validators/Booking/UpdateBookingAvailabilityRequestValidator.cs
@@ -1,36 +1,45 @@
 using AISEA.ApiService.SHARED.DTOs.Requests.Booking;
 using FluentValidation;
 
-namespace AISEA.ApiService.BAL.Validators.Booking;
-
-public class UpdateBookingAvailabilityRequestValidator : AbstractValidator<UpdateBookingAvailabilityRequest>
+namespace AISEA.ApiService.BAL.Validators.Booking
 {
-    public UpdateBookingAvailabilityRequestValidator()
+    public class UpdateBookingAvailabilityRequestValidator : AbstractValidator<UpdateBookingAvailabilityRequest>
     {
-        RuleFor(x => x.StartTime)
-            .Must(BeValidTimeSpan).WithMessage("StartTime must be a valid time between 00:00:00 and 23:59:59.")
-            .NotNull().WithMessage("StartTime is required.");
+        public UpdateBookingAvailabilityRequestValidator()
+        {
+            RuleFor(x => x.StartTime)
+                .Must(BeValidTimeSpan).WithMessage("StartTime must be a valid time between 00:00:00 and 23:59:59.")
+                .NotNull().WithMessage("StartTime is required.");
 
-        RuleFor(x => x.EndTime)
-            .Must(BeValidTimeSpan).WithMessage("EndTime must be a valid time between 00:00:00 and 23:59:59.")
-            .NotNull().WithMessage("EndTime is required.");
+            RuleFor(x => x.EndTime)
+                .Must(BeValidTimeSpan).WithMessage("EndTime must be a valid time between 00:00:00 and 23:59:59.")
+                .NotNull().WithMessage("EndTime is required.");
 
-        RuleFor(x => x)
-            .Must(x => RoundToMinute(x.EndTime) > RoundToMinute(x.StartTime))
-            .WithMessage("EndTime must be after StartTime after rounding to the nearest minute.");
+            RuleFor(x => x)
+                .Must(x => RoundToMinute(x.EndTime) > RoundToMinute(x.StartTime))
+                .WithMessage("EndTime must be after StartTime after rounding to the nearest minute.");
 
-        RuleFor(x => x.DayInWeek)
-            .IsInEnum().WithMessage("DayInWeek must be a valid day of the week (Monday to Sunday).");
+            RuleFor(x => x.DayInWeek)
+                .IsInEnum().WithMessage("DayInWeek must be a valid day of the week (Monday to Sunday).");
 
-    }
+            // Enforce time range to be exactly 30min or 1h
+            RuleFor(x => x)
+                .Must(x =>
+                {
+                    var duration = RoundToMinute(x.EndTime) - RoundToMinute(x.StartTime);
+                    return duration == TimeSpan.FromMinutes(30) || duration == TimeSpan.FromHours(1);
+                })
+                .WithMessage("The time range must be exactly 30 minutes or exactly 1 hour.");
+        }
 
-    private bool BeValidTimeSpan(TimeSpan time)
-    {
-        return time >= TimeSpan.Zero && time < TimeSpan.FromDays(1);
-    }
+        private bool BeValidTimeSpan(TimeSpan time)
+        {
+            return time >= TimeSpan.Zero && time < TimeSpan.FromDays(1);
+        }
 
-    private TimeSpan RoundToMinute(TimeSpan time)
-    {
-        return new TimeSpan(time.Hours, time.Minutes, 0);
+        private TimeSpan RoundToMinute(TimeSpan time)
+        {
+            return new TimeSpan(time.Hours, time.Minutes, 0);
+        }
     }
 }

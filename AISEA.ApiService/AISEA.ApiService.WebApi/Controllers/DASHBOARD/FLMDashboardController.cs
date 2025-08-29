942142a6c9ff0d1d0826714063680d7b1b1b3be3
Ngô Khôi <khoingo2003@gmail.com>
2025-08-29 23:07:08 +0700

    Add Template Dashboard apis

---
+++ AISEA.ApiService/AISEA.ApiService.WebApi/Controllers/DASHBOARD/FLMDashboardController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AISEA.ApiService.WebApi.Controllers.DASHBOARD
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FLMDashboardController : ControllerBase
+    {
+        
+    }
+}

d1de20b24edd0633595cc1444afe9151c1172b0b
Ngô Khôi <khoingo2003@gmail.com>
2025-08-31 00:11:02 +0700

    Skip get metric data of account git

---
+++ AISEA.ApiService/AISEA.ApiService.DAL/Migrations/20250830165649_removenonuse16.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AISEA.ApiService.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class removenonuse16 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GitAccountUsername",
+                table: "StudentProfile");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GitAccountUsername",
+                table: "StudentProfile",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}

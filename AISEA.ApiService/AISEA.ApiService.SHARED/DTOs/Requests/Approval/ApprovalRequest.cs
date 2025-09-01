f2d99576275f02bb20c37a23745a296011f9896a
drekhai123 <khai.lumberjack@gmail.com>
2025-09-01 12:52:52 +0700

    fix Enum to String in Approval

--- AISEA.ApiService/AISEA.ApiService.SHARED/DTOs/Requests/Approval/ApprovalRequest.cs
+++ AISEA.ApiService/AISEA.ApiService.SHARED/DTOs/Requests/Approval/ApprovalRequest.cs
@@ -1,9 +1,11 @@
 using AISEA.ApiService.SHARED.Const.Enums;
+using System.Text.Json.Serialization;
 
 namespace AISEA.ApiService.SHARED.DTOs.Requests.Approval
 {
     public class ApprovalRequest
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public EApprovalStatus ApprovalStatus { get; set; }
         public string? RejectionReason { get; set; }
     }
